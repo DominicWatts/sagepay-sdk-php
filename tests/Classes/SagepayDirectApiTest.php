507c00613253d5efecedeb39cd0c92976804cd88
Dominic Watts <dominic.watts@xigen.co.uk>
2021-03-12 13:40:12 +0000

    Test Direct API

--- tests/Classes/SagepayDirectApiTest.php
+++ tests/Classes/SagepayDirectApiTest.php
@@ -23,6 +23,22 @@ class SagepayDirectApiTest extends TestCase
         $url = 'https://test.sagepay.com/gateway/service/vspdirect-register.vsp';
         self::assertIsString($vpsDirectUrl);
         self::assertSame($vpsDirectUrl, $url);
+
+        $liveUrl = 'https://live.sagepay.com/gateway/service/vspdirect-register.vsp';
+        $api->setVpsDirectUrl($liveUrl);
+        $vpsDirectUrl = $api->getVpsDirectUrl();
+        self::assertIsString($vpsDirectUrl);
+        self::assertSame($vpsDirectUrl, $liveUrl);
+    }
+
+    public function testSetIntegrationMethod()
+    {
+        $payment = new Payment(Payment::DIRECT);
+        $api = $payment->getApi();
+        $api->setIntegrationMethod(Payment::DIRECT);
+        $integrationMethod = $api->getIntegrationMethod();
+        self::assertIsString($integrationMethod);
+        self::assertSame($integrationMethod, Payment::DIRECT);
     }
 
     /**
