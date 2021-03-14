56989ca9c9a0c90832b44eaff70bf85d580d7998
Dominic Watts <dominic.watts@hotmail.co.uk>
2021-03-14 00:58:34 +0000

    Tests Create Request

--- tests/Classes/SagepayCommonTest.php
+++ tests/Classes/SagepayCommonTest.php
@@ -2,11 +2,20 @@
 
 namespace Classes;
 
+use Xigen\Library\Sagepay\Classes\SagepayBasket;
 use Xigen\Library\Sagepay\Classes\SagepayCommon;
+use Xigen\Library\Sagepay\Classes\SagepayCustomer;
+use Xigen\Library\Sagepay\Classes\SagepayCustomerDetails;
+use Xigen\Library\Sagepay\Classes\SagepayItem;
+
+use Xigen\Library\Sagepay\Payment;
 use PHPUnit\Framework\TestCase;
 
 class SagepayCommonTest extends TestCase
 {
+    protected $preserveGlobalState = FALSE;
+    protected $runTestInSeparateProcess = TRUE;
+
     /**
      * Generate a unique VendorTxId
      *
@@ -39,5 +48,160 @@ class SagepayCommonTest extends TestCase
         self::assertNotSame($code, $txCode);
         self::assertSame($code, $orderId);
         self::assertIsString($orderId);
+    }
+
+    /**
+     * Generate values for payment.
+     * Ensure that post data is setted to request with SagepayAbstractApi::setData()
+     *
+     * @return array The response from Sage Pay
+     */
+    public function testFormCreateRequest()
+    {
+        $this->expectError();
+        $this->initialiseConfig();
+
+        $this->payment = new Payment(
+            Payment::FORM,
+            $this->config
+        );
+
+        $this->api = $this->payment->getApi();
+
+        $this->setCustomer();
+        $this->setCustomerDetails();
+        $this->setBasket();
+
+        $sagepayParams = $this->api->createRequest();
+
+        self::assertIsArray($sagepayParams);
+    }
+
+    /**
+     * Generate values for payment.
+     * Ensure that post data is setted to request with SagepayAbstractApi::setData()
+     *
+     * @return array The response from Sage Pay
+     */
+    public function testServerCreateRequest()
+    {
+        $this->initialiseConfig();
+
+        $this->payment = new Payment(
+            Payment::SERVER,
+            $this->config
+        );
+
+        $this->api = $this->payment->getApi();
+
+        $this->setCustomer();
+        $this->setCustomerDetails();
+        $this->setBasket();
+
+        $sagepayParams = $this->api->createRequest();
+
+        self::assertIsArray($sagepayParams);
+    }
+
+    /**
+     * Generate values for payment.
+     * Ensure that post data is setted to request with SagepayAbstractApi::setData()
+     *
+     * @return array The response from Sage Pay
+     */
+    public function testDirectCreateRequest()
+    {
+        $this->expectError();
+        $this->$this->initialiseConfig();
+
+        $this->payment = new Payment(
+            Payment::DIRECT,
+            $this->config
+        );
+
+        $this->api = $this->payment->getApi();
+
+        $this->setCustomer();
+        $this->setCustomerDetails();
+        $this->setBasket();
+
+        $sagepayParams = $this->api->createRequest();
+
+        self::assertIsArray($sagepayParams);
+    }
+
+    /***
+     * Load configs
+     * @return void
+     */
+    protected function initialiseConfig()
+    {
+        $this->config = [
+            'vendorName' => 'test',
+            'env' => 'test',
+            'txType' => 'PAYMENT',
+            'siteFqdns' => [
+                'test' => 'test',
+                'live' => 'test',
+            ],
+            'formSuccessUrl' => 'success',
+            'formFailureUrl' => 'failure',
+            'formPassword' => [
+                'test' => 'test',
+                'live' => 'test',
+            ],
+            'website' => 'http://test.com',
+        ];
+    }
+
+    /**
+     * Add customer ID
+     * @return void
+     */
+    protected function setCustomer()
+    {
+        $customer = new SagepayCustomer();
+        $customer->setCustomerId(123);
+        $this->api->setCustomer($customer);
+    }
+
+    /**
+     * Add address details
+     * @return void
+     */
+    protected function setCustomerDetails()
+    {
+        $customerDetails = new SagepayCustomerDetails();
+        $customerDetails->setFirstname('test');
+        $customerDetails->setLastname('test');
+        $customerDetails->setAddress1('test');
+        $customerDetails->setAddress2('test');
+        $customerDetails->setCity('test');
+        $customerDetails->setState('test');
+        $customerDetails->setPostcode('test');
+        $customerDetails->setCountry('GB');
+        $customerDetails->setPhone('01234567890');
+        $customerDetails->setEmail('test@test.com');
+        $this->api->setCustomerDetails($customerDetails);
+    }
+
+    /**
+     * Build basket
+     * @return void
+     */
+    protected function setBasket()
+    {
+        $item = new SagepayItem();
+        $item->setDescription('test');
+        $item->setProductSku('test');
+        $item->setProductCode('test');
+        $item->setQuantity(1);
+        $item->setUnitNetAmount((float) 1);
+        $item->setUnitTaxAmount(0);
+
+        $basket = new SagepayBasket();
+        $basket->addItem($item);
+        $basket->setDescription('test');
+        $this->api->setBasket($basket);
     }
 }
